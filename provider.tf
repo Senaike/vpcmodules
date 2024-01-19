c4131b18e544f87f4295245b515e7d15f55a2b2f
Abbey senaik <129786312+Senaike@users.noreply.github.com>
2024-01-19 11:22:38 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,15 @@ terraform {
     version = "5.16.1"
   }
  }
+ user_data = <<-EOF
+      #!/bin/sh
+      sudo apt-get update
+      sudo apt install -y apache2
+      sudo systemctl status apache2
+      sudo systemctl start apache2
+      sudo chown -R $USER:$USER /var/www/html
+      sudo echo "<html><body><h1>Hello this custom page built with Terraform User Data</h1></body></html>" > /var/www/html/index.html
+      EOF
 }
 provider "aws" {
     region = "us-east-1"
